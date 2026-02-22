9838ed229b8b2519d2fd9becfe20741e5bcf2b0a
ZillionFundz <123439025+ZillionFundz@users.noreply.github.com>
2026-02-22 09:53:02 -0800

    Update script.js

--- testsignup/Scripts/script.js
+++ testsignup/Scripts/script.js
@@ -2,6 +2,7 @@ const checkBox = document.getElementById("checked");
 const passrord = document.getElementById("passrord");
 const bgDrop = document.getElementById("bg-drop");
 const submitBtn = document.getElementById("submit-btn");
+const signinButton = document.getElementById("signin-button");
 
 checkBox.addEventListener("change", () => {
   if (checkBox.checked) {
@@ -11,4 +12,16 @@ checkBox.addEventListener("change", () => {
     passrord.type = "password";
     return;
   }
+});
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+signinButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  bgDrop.style.transform = "translateY(-150%)";
+
+  setTimeout(() => {
+    bgDrop.style.display = "none";
+  }, 600); // match CSS transition time
 });
