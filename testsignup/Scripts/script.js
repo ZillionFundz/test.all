69cdfd391c15a93dcc848e317f3d8ddb54c3bb2c
ZillionFundz <123439025+ZillionFundz@users.noreply.github.com>
2026-03-01 11:58:41 -0800

    Refactor signup script and update event listeners

--- testsignup/Scripts/script.js
+++ testsignup/Scripts/script.js
@@ -1,15 +1,38 @@
-const checkBox = document.getElementById("checked");
+const signupBtn = document.getElementById("signup-btn");
+const previewButton = document.getElementById("preview-button");
+const signupButton = document.getElementById("signup-button");
+const checkBox = document.getElementById("signup-checkbox");
+const signinButton = document.getElementById("signin-button");
+const switchtoSigninButton = document.getElementById("switchto-signin-button");
+const switchtoSignupButton = document.getElementById("switchto-signup-button");
+const signupForm = document.getElementById("signup-form");
+const signinForm = document.getElementById("signin-form");
+const username = document.getElementById("signup-username");
+const email = document.getElementById("signup-email");
+const password = document.getElementById("signup-password");
+const signupTitle = document.getElementById("signup-title");
 const bgDrop = document.getElementById("bg-drop");
-const previewBtn = document.getElementById("preview-btn");
-const signupButton = document.getElementById("signin-button");
+const signinTitle = document.getElementById("signin-title");
 const detailGroup = document.getElementById("detail-group");
 const detailBox = document.getElementById("detail-box");
-const signTitle = document.getElementById("sign-title");
 const previewUsername = document.getElementById("preview-username");
 const previewEmail = document.getElementById("preview-email");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+console.log(signupBtn);
+
+signupForm.classList.add("hidden-view");
+signinForm.classList.add("hidden-view");
+
+signupBtn.addEventListener("click", () => {
+  // e.preventDefault();
+  signupForm.classList.remove("hidden-view");
+  signupForm.classList.add("active-view");
+
+  setTimeout(() => {
+    bgDrop.classList.add("hide-bg");
+    signupForm.classList.remove("hidden-view");
+    signupForm.classList.add("active-view");
+  }, 1000); // match CSS transition time
+});
 
 checkBox.addEventListener("change", () => {
   if (checkBox.checked) {
@@ -21,19 +44,10 @@ checkBox.addEventListener("change", () => {
   }
 });
 
-signupButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  bgDrop.style.transform = "translateY(-150%)";
-  setTimeout(() => {
-    bgDrop.style.display = "none";
-    signTitle.style.display = "block";
-  }, 600); // match CSS transition time
-});
-
 let isPreview = true;
 let isAnimating = false;
 
-previewBtn.addEventListener("click", () => {
+previewButton.addEventListener("click", () => {
   if (isAnimating) return;
 
   previewUsername.textContent = username.value;
@@ -46,7 +60,7 @@ previewBtn.addEventListener("click", () => {
   setTimeout(() => {
     detailBox.style.opacity = "1";
     detailBox.style.transform = "translateY(0)";
-  }, 50);
+  }, 200);
 
   if (isPreview) {
     console.log("Condition Passed.");
@@ -56,20 +70,23 @@ previewBtn.addEventListener("click", () => {
 
       detailGroup.style.background =
         "linear-gradient(-160deg, #cf18fd, #1100ff, #fff, #000)";
-      previewBtn.textContent = "My Courses";
+      previewButton.textContent = "My Courses";
 
       isPreview = false;
       isAnimating = false;
     }, 3000);
   } else {
-    previewBtn.textContent = "Preview";
+    previewButton.textContent = "Preview";
     isPreview = true;
 
     setTimeout(() => {
+      bgDrop.classList.add("active-view");
+      signupForm.classList.remove("active-view");
+      signupForm.classList.add("hidden-view");
       detailBox.style.display = "none";
-      bgDrop.style.display = "block";
-      bgDrop.style.transform = "translateY(0)";
-      signTitle.style.display = "none";
+      bgDrop.classList.remove("hide-bg");
+      signinTitle.style.display = "none";
+      signupTitle.style.display = "none";
 
       detailBox.style.opacity = "0";
       detailBox.style.transform = "translateY(-20)";
@@ -80,4 +97,18 @@ previewBtn.addEventListener("click", () => {
       isAnimating = false;
     }, 5000);
   }
+});
+
+switchtoSigninButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  signupForm.classList.remove("active-view");
+  signupForm.classList.add("hidden-view");
+  signinForm.classList.add("active-view");
+});
+
+switchtoSignupButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  signinForm.classList.remove("active-view");
+  signinForm.classList.add("hidden-view");
+  signupForm.classList.add("active-view");
 });
